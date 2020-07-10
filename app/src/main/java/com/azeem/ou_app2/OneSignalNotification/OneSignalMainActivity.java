ed93205c1971f99438f2acad640749c7c8af0d4a
mohammed-azeemuddin <mohammedazeemuddin98@gmail.com>
2020-07-10 19:59:14 +0530

    Added Handler for Notification

--- app/src/main/java/com/azeem/ou_app2/OneSignalNotification/OneSignalMainActivity.java
+++ app/src/main/java/com/azeem/ou_app2/OneSignalNotification/OneSignalMainActivity.java
@@ -1,24 +1,56 @@
 package com.azeem.ou_app2.OneSignalNotification;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Application;
+import android.content.Intent;
+import android.util.Log;
 
-import android.os.Bundle;
-
-import com.azeem.ou_app2.R;
+import com.azeem.ou_app2.MainScreen.MainActivity;
+import com.onesignal.OSNotificationOpenResult;
 import com.onesignal.OneSignal;
 
-public class OneSignalMainActivity extends AppCompatActivity {
+import org.json.JSONObject;
+
+public class OneSignalMainActivity extends Application {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_one_signal_main);
+    public void onCreate() {
+        super.onCreate();
 
-        // OneSignal Initialization
         OneSignal.startInit(this)
+                .setNotificationOpenedHandler(new ExampleNotificationOpenedHandler())
                 .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
                 .unsubscribeWhenNotificationsAreDisabled(true)
                 .init();
 
+    }
+
+    private class ExampleNotificationOpenedHandler implements OneSignal.NotificationOpenedHandler {
+
+        // This fires when a notification is opened by tapping on it.
+        @Override
+        public void notificationOpened(OSNotificationOpenResult result) {
+
+            JSONObject data = result.notification.payload.additionalData;
+
+            String customKey = null;
+            String openURL = null;
+            Object activityToLaunch = MainActivity.class;
+
+            if (data != null) {
+                customKey = data.optString("customkey", null);
+                openURL = data.optString("openURL", null);
+            }
+
+            // The following can be used to open an Activity of your choice.
+            // Replace - getApplicationContext() - with any Android Context.
+            // Intent intent = new Intent(getApplicationContext(), YourActivity.class);
+
+            Intent intent = new Intent(OneSignalMainActivity.this, (Class<?>) activityToLaunch);
+            intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.putExtra("openURL", openURL);
+            Log.i("OneSignalExample", "openURL = " + openURL);
+            startActivity(intent);
+
+        }
     }
 }
